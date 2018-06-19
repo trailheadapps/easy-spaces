d29a11783b989a52d1dcbbcc67b2a08c8d84bd48
muenzpraeger <rwinkelmeyer@salesforce.com>
2018-06-19 15:16:40 +0200

    Updated / removed scripts

--- scripts/orgInit.sh
+++ scripts/orgInit.sh
@@ -6,9 +6,11 @@ if [ "$#" -eq 1 ]; then
   DURATION=$1
 fi
 
-sfdx force:org:create -a e -s -f config/project-scratch-def.json -f DURATION
+sfdx force:org:create -a easyspaces -s -f config/project-scratch-def.json -d $DURATION
 sfdx force:source:push
-# sfdx force:user:permset:assign -n e
-# sfdx force:data:tree:import --plan ./data/TheObject__c-plan.json
+sfdx force:user:permset:assign -n EasySpacesObjects
+sfdx force:user:permset:assign -n SpaceManagementApp
+sfdx force:data:tree:import --plan ./data/Plan1.json
+sfdx force:data:tree:import --plan ./data/Plan2.json
 sfdx force:org:open -p /lightning/page/home
 echo "Org is set up"
--- scripts/releaseUpgrade.sh
+++ scripts/releaseUpgrade.sh
@@ -2,10 +2,8 @@
 
 sfdx force:project:upgrade -f
 sfdx muenzpraeger:source:api:set
-sfdx force:org:create -a <appalias> -s -f config/project-scratch-def.json -f 7
+sfdx force:org:create -a es-release-test -s -f config/project-scratch-def.json -d 7
 sfdx force:source:push
-#sfdx force:user:permset:assign -n <apppermset>
-#sfdx force:data:tree:import --plan ./data/TheObject__c-plan.json
 sfdx force:apex:test:run
 sfdx force:org:open
 echo "Org is set up"
