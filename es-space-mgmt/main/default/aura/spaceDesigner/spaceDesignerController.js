deaeb634635a99e51520b02376f8a62f8677dd35
Zayne Turner <16801980+zaynelt@users.noreply.github.com>
2018-10-02 16:39:31 -0700

    Adding support for NavigationItemAPI, cleaning up console actions

--- es-space-mgmt/main/default/aura/spaceDesigner/spaceDesignerController.js
+++ es-space-mgmt/main/default/aura/spaceDesigner/spaceDesignerController.js
@@ -1,13 +1,4 @@
 ({
-    onInit : function(component, event, helper){
-        var workspaceAPI = component.find("workspace");
-        workspaceAPI.getEnclosingTabId().then(function(tabId) {
-            component.set("v.tabId", tabId);
-       })
-        .catch(function(error) {
-            console.log(error);
-        }); 
-    },
     manageFlow : function(component, event, helper) {
         var channel = event.getParam("channel");
         var record = component.get("v.reservation");
@@ -20,12 +11,12 @@
 
     handleReset : function(component, event, helper){
         if(event.getParam("status") === "FINISHED"){
-            var workspaceAPI = component.find("workspace");
-            var currentTabId = component.get("v.tabId");
-            workspaceAPI.refreshTab({
-                tabId: currentTabId,
-                includeAllSubtabs: false
-            }).catch(function(error) {
+            console.log('flow finish detected');
+            var navigationItemAPI = component.find("navigationItemAPI");
+            navigationItemAPI.refreshNavigationItem().then(function(response) {
+                console.log('navRefresh',response);
+            })
+            .catch(function(error) {
                 console.log(error);
             });
         }
